b00eaf44271a8c35c62449649958857fff84abbe
reagancheatham <reagancheatham@gmail.com>
2024-04-22 20:13:11 -0500

    small fixes

--- Assets/Scripts/Util/LookAtCamera.cs
+++ Assets/Scripts/Util/LookAtCamera.cs
@@ -7,13 +7,12 @@ public class LookAtCamera : MonoBehaviour
     [SerializeField] private bool invert;
     private Transform target;
 
-    private void Awake()
-    {
-        target = Camera.main.transform;
-    }
-
     private void Update()
     {
+        if (target != Camera.main.transform)
+            target = Camera.main.transform;
+
+
         if (invert)
         {
             Vector3 direction = -(target.position - transform.position);
